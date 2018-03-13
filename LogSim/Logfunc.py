27046a16dd089dd1c74ae3ea6af8ccccf56de09c
SebastianB1999 <34735935+SebastianB1999@users.noreply.github.com>
2018-03-13 12:11:22 +0100

    private Attribute

--- LogSim/Logfunc.py
+++ LogSim/Logfunc.py
@@ -6,20 +6,38 @@ class ANDGATE(object):
 
 
     def __init__(self, *args, **kwargs):
-        self.Input0 = False
-        self.Input1 = False
-        self.Output = False
-        self.Name = "AndGate"
+        self.__Input0 = False
+        self.__Input1 = False
+        self.__Output = False
+        self.__Name = "AndGate"
         return super().__init__(*args, **kwargs)
 
     def execute(self):
-        if Input0 == True:
-            if INput1 == True:
-                self.Output = False
+        if __Input0 == True:
+            if __Input1 == True:
+                self.__Output = False
 
     def show(self):
-        print("Die Bedingungen sind: "+__str__(self.Output))
+        print("Die Bedingungen sind: "+__str__(self.__Output))
 
     def __str__(self,wert):
         return str(wert)
         return super().__str__()
+
+    # property()
+
+    def setzeInput0(self,wert):
+        self.__Input0 = wert
+
+    def setzeInput1(self,wert):
+        self.__Input1 = wert
+
+    def gebeInput0(self):
+        return self.__Input0
+
+    def gebeInput1(self):
+        return self.__Input1
+
+    def gebeName(self):
+        return self.__Name
+
